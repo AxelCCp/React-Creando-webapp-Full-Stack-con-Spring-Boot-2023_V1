887d8b1ce1957f80a38ceb3cb59470d76ad7c27d
axelccp <hpmajin@outlook.es>
2023-07-16 22:20:08 -0600

    67

--- 5-invoice-app-form/src/InvoiceApp.jsx
+++ 5-invoice-app-form/src/InvoiceApp.jsx
@@ -4,11 +4,19 @@ import { CompanyView } from "./components/CompanyView";
 import { InvoiceView } from "./components/InvoiceView";
 import { TableItemsView } from "./components/TableItemsView";
 import { TotalView } from "./components/TotalView";
+import { useState } from "react";
 
 export const InvoiceApp = () => {
 
-    //DESESTRUCTURACIÓN
-    const {total, id, name, client, company, items} = getInvoice();
+    const {total, id, name, client, company, items: itemsInitial} = getInvoice();
+
+    const[productValue, setProductValue] = useState('');
+    const[priceValue, setPriceValue] = useState('');
+    const[quantityValue, setQuantityValue] = useState('');
+
+    const[items, setItems] = useState(itemsInitial); 
+
+    const[counter, setCounter] = useState(4);
 
     return (
         <>
@@ -44,6 +52,40 @@ export const InvoiceApp = () => {
 
                        /*TOTAL*/
                        <TotalView total= {total}/>
+
+                       <form className="w-50" onSubmit={event => {
+                            event.preventDefault();                                                                                 //PREVIENE LA PERDIDA DE INFO SI ES Q SE RECARGA LA PAGINA. 
+                            
+                            if(productValue.trim().length <= 1) return;
+                            if(priceValue.trim().length <= 1) return;
+                            if(isNaN(priceValue.trim())) {
+                                alert('El valor ingersado no es un número');    
+                                return;
+                            }
+                            if(quantityValue.trim().length < 1) return;
+                            if(isNaN(quantityValue.trim())) {
+                                alert('El valor ingersado no es un número');  
+                                return;
+                            }       
+                            setItems([...items, {                                                                                   //AGREGA UN NUEVO ELEMENTO  AL ARRAY DE ELEMENTOS.
+                                id:counter, 
+                                product:productValue.trim(),                                                                        //SE QUITAN LOS ESPACIOS EN BLANCO A LA IZQ Y DER. 
+                                price:parseInt(priceValue, 10), 
+                                quantity: parseInt(quantityValue, 10)
+                            }]);
+                            //SE LIMPIAN LOS DATOS DEL FOMULARIO DESPUES DE AGREGAR EL PRODUCTO.
+                            setProductValue('');
+                            setPriceValue('');
+                            setQuantityValue('');
+                            setCounter(counter+1);
+                       }}>
+                            <input type="text" name="product" placeholder="Producto" value={productValue} className="form-control m-3" onChange={event => {console.log(event.target.value); setProductValue(event.target.value); }}/>
+                            <input type="number" name="price" placeholder="Precio" value={priceValue} className="form-control m-3" onChange={event => {console.log(event.target.value); setPriceValue(event.target.value); }}/>
+                            <input type="number" name="quantity" placeholder="Cantidad" value={quantityValue} className="form-control m-3" onChange={event => {console.log(event.target.value); setQuantityValue(event.target.value); }}/>
+                       
+                            <button type="submit" className="btn btn-primary m-3">Nuevo item</button>
+                       
+                       </form>
 
                     </div>
 
